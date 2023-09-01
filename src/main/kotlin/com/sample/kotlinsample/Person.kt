a01e8fe29d0b38f1951e60b482eabb29034942a9
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-09-01 18:21:59 +0900

    init: 🎉 seal, extended 학습
    이 때, 이전에 사용된 것들이 있으면 이에 대한 사이드 이펙트 체크를 해준다.
    이 점이 확실히 에러의 발생을 방지해주는 강력한 기능인 것으로 보인다.

--- src/main/kotlin/com/sample/kotlinsample/Person.kt
+++ src/main/kotlin/com/sample/kotlinsample/Person.kt
@@ -1,6 +1,42 @@
 package com.sample.kotlinsample
 
-class Person (
+import com.sample.kotlinsample.Division.*;
+sealed class Person(
     val name: String,
-    val isMarried: Boolean
+    var isMarried: Boolean
 )
+
+class Developer(
+    name: String,
+    isMarried: Boolean,
+    val language: String
+) : Person(
+    name,
+    isMarried
+)
+
+class Marketer (
+    name: String,
+    isMarried: Boolean,
+    val division: Division
+) : Person(
+    name,
+    isMarried
+)
+
+enum class Division{
+    CONTENTS, DIGITAL, BRAND
+}
+
+fun printSpecialSkill(person: Person) =
+    when(person){
+    is Developer -> person.language
+    is Marketer -> person.division
+}
+
+fun main(){
+    val developer = Developer("아무로", true, "Nihongo")
+    val marketer = Marketer("카스발", false, CONTENTS)
+    println("developer`s special skill is : ${printSpecialSkill(developer)}")
+    println("marketer`s special skill is : ${printSpecialSkill(marketer)}")
+}
