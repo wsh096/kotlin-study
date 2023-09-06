ac4d89c1b9d399fe68e6d752722f1d5fdaa1b109
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-09-06 15:07:04 +0900

    init: 🎉 default 파라미터 예시
    1. default 출력
    2. 값 바뀌기
    3. 지정 값 바뀌

--- src/main/kotlin/com/sample/kotlinsample/이름붙인인자.kt
+++ src/main/kotlin/com/sample/kotlinsample/이름붙인인자.kt
@@ -5,12 +5,17 @@ fun getMyName(
     lastName: String
 ): String = "$firstName $lastName"
 
-fun main(arg: Array<String>){
+fun main(){
     println(getMyName("석현", "왕"))
     //파라미터가 많아지면 무엇인지 알 수 없어 혼동이 올 수 있기 떄문에, 이름 명시도 가능
     println(getMyName(lastName = "왕", firstName = "석현"))
+    println(getMyNameDefault())//defalut foo bar 나옴
+    println(getMyNameDefault("값이 변경됨"))
+    println(getMyNameDefault(lName = "변경값 지정"))
 }
 
 //default 파라미터 예시
-
-//fun getMyNameDefault()
+fun getMyNameDefault(
+    fName: String = "foo",
+    lName: String = "bar"
+): String = "$fName $lName"
